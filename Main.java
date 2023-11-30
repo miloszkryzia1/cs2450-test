8a8445e2f8511e56c56f133e1a845420d615dd45
cmur13 <119700959+cmur13@users.noreply.github.com>
2023-11-29 22:49:47 -0800

    Revert "j"

--- Main.java
+++ Main.java
@@ -1,38 +1,30 @@
-package com.example.testing3;
-
+package application;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 public class Main extends Application {
-    int currentIndex =0;
+    Label response;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage stage) {
-        // background image
-        Scene scene = new Scene(homePage(stage));
-        stage.setScene(scene);
-        stage.setFullScreen(true);
-        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);  // ctrl esc
-        stage.setResizable(true);
-        stage.show();
-    }
-    public VBox homePage(Stage stage){
 
-        MenuBar menuBar = new MenuBar();
+        MenuBar mb = new MenuBar();
         // create the products menu
+
         Menu products = new Menu("Products");
         MenuItem apron = new MenuItem("Lead Apron");
         MenuItem radiation = new MenuItem("Radiation Reducin");
@@ -53,6 +45,7 @@ public class Main extends Application {
 
         });
 
+
         // create the equipments menu
         Menu equipment = new Menu("Equipment");
         MenuItem item2 = new MenuItem("Small Animal immobilizers");
@@ -68,8 +61,6 @@ public class Main extends Application {
         Menu parts = new Menu("Parts");
         MenuItem MRI2 = new MenuItem("MRI");
         parts.getItems().addAll(MRI2);
-
-
 
         // create contact
         Label contactLabel = new Label("Contact us");
@@ -87,8 +78,8 @@ public class Main extends Application {
             // info
         });
 
-        menuBar.getMenus().addAll(products, equipment, supplies, parts, about, contactMenu);
-        menuBar.setPadding(new Insets(10));
+        mb.getMenus().addAll(products, equipment, supplies, parts, about, contactMenu);
+        mb.setPadding(new Insets(10));
 
         // add company name
 
@@ -102,79 +93,16 @@ public class Main extends Application {
         });
 
          */
-        // search bar
-        TextField search = new TextField();
-        Button searchButton = new Button("Go!");
-        HBox searchBox = new HBox(search, searchButton);
 
-        // most popular section
-        Label mostPopular = new Label("Most Popular");
-        mostPopular.setStyle("-fx-font-size: 24; -fx-text-fill: BLACK;");
-        // create an array of product images
-        Image[] images = new Image[4];
-        images[0] = new Image("file:./res/test.png");
-        images[1] = new Image("file:./res/test2.png");
-        images[2] = new Image ("file:./res/test3.png");
-        images[3] = new Image("file:./res/test4.png");
-        ImageView imageView = new ImageView(images[0]);
-        imageView.setFitWidth(200);
-        imageView.setFitHeight(400);
-        imageView.setPreserveRatio(true);
 
-        // create the anchorPane
-        AnchorPane root = new AnchorPane();
-        AnchorPane.setTopAnchor(imageView, 50.0);
-        AnchorPane.setLeftAnchor(imageView, 200.0); // change later
-
-        // create buttons on each side of the image
-        Button leftButton = new Button("<");
-        leftButton.setStyle("-fx-font-size: 10; -fx-text-fill: white; -fx-background-color: black;");
-        Button rightButton = new Button(">");
-        rightButton.setStyle("-fx-font-size: 10; -fx-text-fill: white; -fx-background-color: black;");
-        // Position the buttons
-        AnchorPane.setTopAnchor(leftButton, 200.0);
-        AnchorPane.setLeftAnchor(leftButton, 50.0);
-        AnchorPane.setTopAnchor(rightButton, 200.0);
-        AnchorPane.setRightAnchor(rightButton, 50.0);
-
-        leftButton.setOnAction(event -> {
-            // Decrement the current index
-            currentIndex--;
-            // Wrap around if the index is out of bounds
-            if (currentIndex < 0) {
-                currentIndex = images.length - 1;
-            }
-            // Set the image view to the new image
-            imageView.setImage(images[currentIndex]);
-        });
-
-        rightButton.setOnAction(e -> {
-            // Increment the current index
-            currentIndex++;
-            // Wrap around if the index is out of bounds
-            if (currentIndex >= images.length) {
-                currentIndex = 0;
-            }
-            // Set the image view to the new image
-            imageView.setImage(images[currentIndex]);
-        });
-        // create a hbox for the images
-        root.getChildren().addAll(rightButton,imageView, leftButton);
-
-        // browse all products
-        Label browse = new Label("Browse all products");
-        Button leadApron = new Button("Lead Apron");
-        Button gloves = new Button("Radiation reducing gloves");
-        Button mri = new Button("Medium MRI Phantom");
-        Button animalImmobilizers = new Button("Small Animal Immobilizers");
-
-        VBox homePage = new VBox( menuBar, searchBox, mostPopular,root, browse);
-        homePage.setAlignment(Pos.CENTER);
-        homePage.setSpacing(10);
-        homePage.setPadding(new Insets(10));
-
-        // return the vbox, FIX LATER
-        return homePage;
+        // testing
+        BorderPane root = new BorderPane();
+        root.setTop(mb);
+        Scene scene = new Scene(root, 600, 600);
+        //Scene scene = new Scene(window1(stage));
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.show();
     }
 }
 
