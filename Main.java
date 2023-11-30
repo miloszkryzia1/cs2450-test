03c4a21048c67a5e17ca1c63e0d77aa8cc023293
cmur13 <cmur131@gmail.com>
2023-11-30 13:00:58 -0800

    .

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-package application;
-
+package com.example.testing3;
 
 import java.util.Scanner;
 
@@ -8,6 +7,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
@@ -15,11 +15,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import java.io.*;
 
@@ -33,6 +30,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         // background image
         Scene scene = new Scene(homePage(stage));
+        scene.getStylesheets().add("Styling.css");
         stage.setScene(scene);
         stage.setFullScreen(true);
         stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);  // ctrl esc
@@ -42,26 +40,6 @@ public class Main extends Application {
     public VBox homePage(Stage stage){
 
         MenuBar menuBar = new MenuBar();
-        // create the products menu
-        Menu products = new Menu("Products");
-        MenuItem apron = new MenuItem("Lead Apron");
-        MenuItem radiation = new MenuItem("Radiation Reducin");
-        MenuItem MRI = new MenuItem("MRI");
-        MenuItem animal = new MenuItem("Small animal immobilizers");
-        products.getItems().addAll(apron, radiation, MRI, animal);
-
-        apron.setOnAction(event->{
-
-        });
-        radiation.setOnAction(event->{
-
-        });
-        MRI.setOnAction(event->{
-
-        });
-        animal.setOnAction(event->{
-
-        });
 
         // create the equipments menu
         Menu equipment = new Menu("Equipment");
@@ -70,12 +48,14 @@ public class Main extends Application {
 
         // create supplies menu
         Menu supplies = new Menu("Supplies");
+        supplies.setStyle("-fx-text-fill: white");
         MenuItem item3 = new MenuItem("Lead Apron");
         MenuItem item4 = new MenuItem("Radiation Reducing Gloves");
         supplies.getItems().addAll(item3, item4);
 
         // create the parts menu
         Menu parts = new Menu("Parts");
+        parts.setStyle("-fx-text-fill: white");
         MenuItem MRI2 = new MenuItem("MRI");
         parts.getItems().addAll(MRI2);
 
@@ -84,6 +64,7 @@ public class Main extends Application {
         // create contact
         Label contactLabel = new Label("Contact us");
         Menu contactMenu = new Menu (null, contactLabel);
+        contactMenu.setStyle("-fx-text-fill: white");
         contactLabel.setOnMouseClicked(event->{
             // info
         });
@@ -92,38 +73,45 @@ public class Main extends Application {
         // create the about
         Label aboutLabel = new Label ("About");
         Menu about = new Menu(null, aboutLabel);
+        about.setStyle("-fx-text-fill: white");
         // set an action when about is clicked
         aboutLabel.setOnMouseClicked(event->{
             // info
         });
 
-        menuBar.getMenus().addAll(products, equipment, supplies, parts, about, contactMenu);
-        menuBar.setPadding(new Insets(10));
+        menuBar.getMenus().addAll(equipment, supplies, parts, about, contactMenu);
+        //menuBar.setPadding(new Insets(10));
+        menuBar.setPrefWidth(stage.getWidth());
+        menuBar.getStyleClass().add("menu-bar");
 
-        // add company name
-
-        /*
-        // home menu, add later
-        Label homeLabel = new Label("Home");
-        Menu homeMenu = new Menu(null, homeLabel;
-        homeLabel.setOnMouseClicked(event->{
-            Scene scene
-
-        });
-
-         */
-        // search bar
+        // search bar area
+        Label name = new Label("Pacific North West X-Ray INC");
+        name.getStyleClass().add("label-name");
         TextField search = new TextField();
         Button searchButton = new Button("Go!");
-        //TEST - TEPORARY
-        searchButton.setOnAction(event -> {
-        	getProductPage("glove.txt", stage);
-        });
-        HBox searchBox = new HBox(search, searchButton);
+        HBox searchBox = new HBox(name, search, searchButton);
+        searchBox.setAlignment(Pos.CENTER);
+        searchBox.setSpacing(10);
+        searchBox.setPadding(new Insets(30,0,0, 0));
+
+
+
+        search.setPromptText("Search");
+        search.setPrefWidth(775);
+        search.setPrefHeight(58);
+        search.setMaxWidth(600);
+        search.getStyleClass().add("search-bar");
+
+        searchButton.setPickOnBounds(true);
+        searchButton.setPrefHeight(55);
+        searchButton.setMaxWidth(100);
+        searchButton.getStyleClass().add("button-search");
+        // -fx-background-color: linear-gradient(to bottom, #cccccc, #999999);
+
 
         // most popular section
         Label mostPopular = new Label("Most Popular");
-        mostPopular.setStyle("-fx-font-size: 24; -fx-text-fill: BLACK;");
+        mostPopular.setStyle("-fx-text-fill: white; -fx-font-size: 20; -fx-font-weight: bold;");
         // create an array of product images
         Image[] images = new Image[4];
         images[0] = new Image("file:./res/test.png");
@@ -135,10 +123,15 @@ public class Main extends Application {
         imageView.setFitHeight(400);
         imageView.setPreserveRatio(true);
 
+        // most popular label
+        AnchorPane.setTopAnchor(mostPopular,50.0);
+        AnchorPane.setLeftAnchor(mostPopular,700.0);
         // create the anchorPane
-        AnchorPane root = new AnchorPane();
-        AnchorPane.setTopAnchor(imageView, 50.0);
-        AnchorPane.setLeftAnchor(imageView, 200.0); // change later
+        AnchorPane mostPopularSection = new AnchorPane();
+
+        // set contraints for imageView
+        AnchorPane.setTopAnchor(imageView, 200.0);
+        AnchorPane.setLeftAnchor(imageView, 600.0); // change later
 
         // create buttons on each side of the image
         Button leftButton = new Button("<");
@@ -146,10 +139,10 @@ public class Main extends Application {
         Button rightButton = new Button(">");
         rightButton.setStyle("-fx-font-size: 10; -fx-text-fill: white; -fx-background-color: black;");
         // Position the buttons
-        AnchorPane.setTopAnchor(leftButton, 200.0);
-        AnchorPane.setLeftAnchor(leftButton, 50.0);
-        AnchorPane.setTopAnchor(rightButton, 200.0);
-        AnchorPane.setRightAnchor(rightButton, 50.0);
+        AnchorPane.setTopAnchor(leftButton, 150.0);
+        AnchorPane.setLeftAnchor(leftButton, 500.0);
+        AnchorPane.setTopAnchor(rightButton, 150.0);
+        AnchorPane.setRightAnchor(rightButton, 500.0);
 
         leftButton.setOnAction(event -> {
             // Decrement the current index
@@ -172,8 +165,8 @@ public class Main extends Application {
             // Set the image view to the new image
             imageView.setImage(images[currentIndex]);
         });
-        // create a hbox for the images
-        root.getChildren().addAll(rightButton,imageView, leftButton);
+        // create a hbox for the images]\
+        mostPopularSection.getChildren().addAll(mostPopular, rightButton, imageView, leftButton);
 
         // browse all products
         Label browse = new Label("Browse all products");
@@ -182,13 +175,34 @@ public class Main extends Application {
         Button mri = new Button("Medium MRI Phantom");
         Button animalImmobilizers = new Button("Small Animal Immobilizers");
 
-        VBox homePage = new VBox( menuBar, searchBox, mostPopular,root, browse);
-        homePage.setAlignment(Pos.CENTER);
-        homePage.setSpacing(10);
-        homePage.setPadding(new Insets(10));
+
+        // background image
+        /*
+        Image background = new Image("file:res/background.jpeg");
+        ImageView backImage = new ImageView(background);
+
+        backImage.fitWidthProperty().bind(stage.widthProperty());
+        backImage.fitHeightProperty().bind(stage.heightProperty());
+
+        backImage.setPreserveRatio(true);
+        backImage.setSmooth(true);
+
+         */
+        Image img = new Image("file:res/background.jpeg");
+        BackgroundSize bgSize = new BackgroundSize(100, 100, true, true, true, true);
+        BackgroundImage bgImg = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, bgSize);
+
+        VBox home = new VBox(menuBar, searchBox, mostPopularSection, browse);
+        home.setBackground(new Background(bgImg));
+
+        home.getStyleClass().add("root");
+        home.setSpacing(10);
+        home.setPadding(new Insets(10));
+
 
         // return the vbox, FIX LATER
-        return homePage;
+        return home;
     }
     
     /**
