59e9ca26a963eefe26b194de5e61924d5fe195a1
miloszkryzia1 <milosz.kryzia@gmail.com>
2023-11-29 16:44:59 -0800

    add file

---
+++ ProductPage.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ProductPage{
+	public static Scene getProductPage() {
+		
+	}
+}
