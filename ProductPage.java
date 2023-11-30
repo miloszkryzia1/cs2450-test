9d4631cbbc910e92a9e4c438b92b6c602723a2f1
ayecmon <112906516+ayecmon@users.noreply.github.com>
2023-11-29 21:33:45 -0800

    added menu and buttons

--- ProductPage.java
+++ ProductPage.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
@@ -31,15 +33,40 @@ public class ProductPage{
 		String pTitle = null;
 		try {
     		String filePath = System.getProperty("user.dir");
-			Scanner scanner = new Scanner(new File("src\\application\\products\\" + filename));
+			Scanner scanner = new Scanner(new File("src/products/" + filename));
 			pTitle = scanner.nextLine();
-			pImage = new Image("file:src\\application\\images\\" + scanner.nextLine());
+			pImage = new Image("file:src/Images/" + scanner.nextLine());
 			pDescription = scanner.nextLine();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		//Menu bar
+        MenuBar mb = new MenuBar();
+        
+        // Home Menu
+        Menu home = new Menu("Home");
+		// create the equipments menu
+        Menu equipment = new Menu("Equipment");
+        MenuItem item2 = new MenuItem("Small Animal immobilizers");
+        equipment.getItems().addAll(item2);
+       
+        // create supplies menu
+        Menu supplies = new Menu("Supplies");
+        MenuItem item3 = new MenuItem("Lead Apron");
+        MenuItem item4 = new MenuItem("Radiation Reducing Gloves");
+        supplies.getItems().addAll(item3, item4);
+
+        // create the parts menu
+        Menu parts = new Menu("Parts");
+        MenuItem MRI2 = new MenuItem("MRI");
+        parts.getItems().addAll(MRI2);
+        
+        //create order form menu
+        Menu form = new Menu("Order Form");
+        
+        mb.getMenus().addAll(home, equipment, supplies, parts, form);
+        mb.setPadding(new Insets(10));
 		//Image
 		ImageView pImgView = new ImageView();
 		pImgView.setImage(pImage);
@@ -49,17 +76,32 @@ public class ProductPage{
 		
 		//Desc
 		Label pDescText = new Label(pDescription);
-		pDescText.setPrefSize(500, 500);
+		pDescText.setPrefSize(400, 400);
 		pDescText.setWrapText(true);
 		
 		//Title
 		Label pTitleLabel = new Label(pTitle);
-		
+		//inline for Title
+		pTitleLabel.setStyle("-fx-font-size: 18pt; -fx-font-weight: bold");
+		//Button
+		Button p_details = new Button("Product Details");
+		Button buy_button = new Button("buy Now!");
 		//Layout
-		HBox hbox = new HBox(20, pImgView, pDescText);
-		VBox vbox = new VBox(20, pTitleLabel, hbox);
+		HBox buttonBox = new HBox(20, p_details, buy_button);
+		buttonBox.setAlignment(Pos.CENTER);
+		VBox vbox1 = new VBox(20, pDescText, buttonBox);
+		vbox1.setAlignment(Pos.TOP_RIGHT);
+		HBox hbox1 = new HBox(20, pImgView, vbox1);
+		hbox1.setAlignment(Pos.CENTER);
+		VBox vbox2 = new VBox(20, pTitleLabel, hbox1);
+		vbox2.setAlignment(Pos.CENTER);
+		VBox vbox3 = new VBox(25,mb, vbox2);
+		vbox3.setAlignment(Pos.TOP_LEFT);
+		//HBox hbox = new HBox(30, pImgView, pDescText);
+		//VBox vbox = new VBox(20, pTitleLabel, hbox);
 		
-		Scene scene = new Scene(vbox,1000,1000);
+		
+		Scene scene = new Scene(vbox3,1000,1000);
 		
 		return scene;
 	}
